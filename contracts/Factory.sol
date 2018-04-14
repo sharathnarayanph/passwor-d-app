69ee02bfb1310f27daff73fd7005a253ba459b39
Sharath Narayan <phsharathnarayan@gmail.com>
2018-04-14 20:55:06 +0530

    Authentication/Creds update/ Signup completed

--- contracts/Factory.sol
+++ contracts/Factory.sol
@@ -11,17 +11,26 @@ contract Factory is Killable {
 
     mapping (address => User) private users;
 
-    modifier validName(bytes32 name) {
-        // Must be a valid user address
+    modifier validCreds(bytes32 name, bytes32 email, bytes32 password) {
+        // Must be valid credentials
 
         require(!(name == 0x0));
+        require(!(email == 0x0));
+        require(!(password == 0x0));
+        _;
+    }
+
+    modifier onlyExistingUser {
+        // Check if user exists or terminate
+
+        require(!(users[msg.sender].name == 0x0));
         _;
     }
 
     function signUp(bytes32 name, bytes32 email, bytes32 password)
     public
     payable
-    validName(name)
+    validCreds(name, email, password)
     returns (bytes32) {
         //Add users name to the hash map
 
@@ -32,5 +41,33 @@ contract Factory is Killable {
         users[msg.sender].email = email;
 
         return (users[msg.sender].name);        
+    }
+
+    function update(bytes32 name, bytes32 email, bytes32 password)
+    public
+    payable
+    validCreds(name, email, password)
+    onlyExistingUser
+    returns (bytes32) {
+        // Update user credentials
+
+        if (users[msg.sender].name != 0x0) {
+            users[msg.sender].name = name;
+        }
+
+        if (users[msg.sender].email != 0x0) {
+            users[msg.sender].email = email;
+        }
+
+        if (users[msg.sender].password != 0x0) {
+            users[msg.sender].password = password;
+        }
+    }
+
+    function login(bytes32 password) constant
+    public
+    returns (bytes32) {
+
+        return (users[msg.sender].name);
     }
 }
